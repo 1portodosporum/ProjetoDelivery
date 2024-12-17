b35bde62507ce382711caed772251e63b78904d2
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-12-17 11:49:42 -0300

    feat: add pedido controller

---
+++ src/main/java/com/generation/projetoDelivery/controller/PedidoController.java
@@ -0,0 +1,72 @@
+package com.generation.projetoDelivery.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.generation.projetoDelivery.model.Pedido;
+import com.generation.projetoDelivery.repository.PedidoRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/pedidos")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class PedidoController {
+    
+    @Autowired
+    private PedidoRepository pedidoRepository;
+    
+    @GetMapping
+    public ResponseEntity<List<Pedido>> getAll(){
+        return ResponseEntity.ok(pedidoRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Pedido> getById(@PathVariable Long id){
+        return pedidoRepository.findById(id)
+            .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @GetMapping("/status/{status}")
+    public ResponseEntity<List<Pedido>> getByTitulo(@PathVariable String status) {
+        return ResponseEntity.ok(pedidoRepository.findAllByStatusContainingIgnoreCase(status));
+    }
+
+    @PostMapping
+    public ResponseEntity<Pedido> post(@Valid @RequestBody Pedido pedido) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(pedidoRepository.save(pedido));
+    }
+
+    @PutMapping
+    public ResponseEntity<Pedido> put(@Valid @RequestBody Pedido pedido){
+        return ResponseEntity.status(HttpStatus.OK)
+        .body(pedidoRepository.save(pedido));
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deletePedido(@PathVariable Long id) {
+        Optional<Pedido> pedido = pedidoRepository.findById(id);
+        if(pedido.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        pedidoRepository.deleteById(id);
+    }
+}
--- src/main/java/com/generation/projetoDelivery/repository/PedidoRepository.java
+++ src/main/java/com/generation/projetoDelivery/repository/PedidoRepository.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.generation.projetoDelivery.model.Produto;
+import com.generation.projetoDelivery.model.Pedido;
 
-public interface PedidoRepository extends JpaRepository<Produto, Long>{
 
-    public List<Produto> findAllByStatusContainingIgnoreCase(String status);
+public interface PedidoRepository extends JpaRepository<Pedido, Long>{
+
+    public List<Pedido> findAllByStatusContainingIgnoreCase(String status);
 }
