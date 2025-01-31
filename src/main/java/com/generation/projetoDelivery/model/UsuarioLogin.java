172a29d5d82ecc67bbf6a32f282ef235c9f219b2
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-01-31 12:31:30 -0300

    :sparkles: feat: add tipo field to UsuarioLogin model

--- src/main/java/com/generation/projetoDelivery/model/UsuarioLogin.java
+++ src/main/java/com/generation/projetoDelivery/model/UsuarioLogin.java
@@ -7,6 +7,8 @@ public class UsuarioLogin {
 	private String usuario;
 	private String senha;
 	private String foto;
+	private String tipo;
+
 	private String token;
 
 	public Long getId() {
@@ -55,6 +57,13 @@ public class UsuarioLogin {
 
 	public void setToken(String token) {
 		this.token = token;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
 	}
 
 }
